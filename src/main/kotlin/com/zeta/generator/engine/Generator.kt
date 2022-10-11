43119fd8448c23e7f99bae9a7be99f0a6eb21aa5
xia5800 <ylwnpzzzn@163.com>
2022-10-11 11:13:50 +0800

    feat: 重构代码
    解决TreeEntity、StateEntity生成的SaveDTO、UpdateDTO缺少字段问题

--- src/main/kotlin/com/zeta/generator/engine/Generator.kt
+++ src/main/kotlin/com/zeta/generator/engine/Generator.kt
@@ -7,6 +7,7 @@ import cn.hutool.log.LogFactory
 import com.baomidou.mybatisplus.annotation.IdType
 import com.baomidou.mybatisplus.generator.FastAutoGenerator
 import com.baomidou.mybatisplus.generator.config.*
+import com.baomidou.mybatisplus.generator.config.po.TableField
 import com.baomidou.mybatisplus.generator.config.rules.DateType
 import com.baomidou.mybatisplus.generator.engine.BeetlTemplateEngine
 import com.zeta.generator.enums.EntityTypeEnum
@@ -20,37 +21,37 @@ object Generator {
     private val log: Log = LogFactory.get();
 
     /** 空字符串，用于join时多拼接一个'/' */
-    private val EMPTY_STR = ""
+    private const val EMPTY_STR = ""
 
     /** Kotlin 文件存放路径 */
-    private val KOTLIN_SOURCE_PATH = "src/main/kotlin"
+    private const val KOTLIN_SOURCE_PATH = "src/main/kotlin"
 
     /** 资源 文件存放路径 */
-    private val RESOURCES_SOURCE_PATH = "src/main/resources"
+    private const val RESOURCES_SOURCE_PATH = "src/main/resources"
 
     /** controller 文件存放路径 */
-    private val CONTROLLER_PATH = "controller"
+    private const val CONTROLLER_PATH = "controller"
 
     /** service 文件存放路径 */
-    private val SERVICE_PATH = "service"
+    private const val SERVICE_PATH = "service"
 
     /** service实现类 文件存放路径 */
-    private val SERVICE_IMPL_PATH = "$SERVICE_PATH/impl"
+    private const val SERVICE_IMPL_PATH = "$SERVICE_PATH/impl"
 
     /** mapper 文件存放路径 */
-    private val MAPPER_PATH = "dao"
+    private const val MAPPER_PATH = "dao"
 
     /** xml 文件存放路径 */
-    private val MAPPER_XML_PATH = "mapper"
+    private const val MAPPER_XML_PATH = "mapper"
 
     /** entity 文件存放路径 */
-    private val ENTITY_PATH = "model/entity"
+    private const val ENTITY_PATH = "model/entity"
 
     /** entityDTO 文件存放路径 */
-    private val ENTITY_DTO_PATH = "model/dto"
+    private const val ENTITY_DTO_PATH = "model/dto"
 
     /** entityQueryParam 文件存放路径 */
-    private val ENTITY_QUERY_PARAM_PATH = "model/param"
+    private const val ENTITY_QUERY_PARAM_PATH = "model/param"
 
 
     /**
@@ -120,30 +121,14 @@ object Generator {
         it.entity("model.entity")
 
         // 配置什么文件生成在什么地方
-        // 包路径  例如：com/zeta/system
-        val packagePath = "${config.packageName.split(".").joinToString("/")}/${config.moduleName}"
-        // basePath: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system】
-        val basePath = listOf(config.outputDir, config.projectName, KOTLIN_SOURCE_PATH, packagePath).joinToString("/")
-        // controller: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】controller/
-        val controllerPath = listOf(basePath, CONTROLLER_PATH, EMPTY_STR).joinToString("/")
-        // entity: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system】/model/entity/
-        val entityPath = listOf(basePath, ENTITY_PATH, EMPTY_STR).joinToString("/")
-        // service: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】service/
-        val servicePath = listOf(basePath, SERVICE_PATH, EMPTY_STR).joinToString("/")
-        // serviceImpl: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】service/impl/
-        val serviceImplPath = listOf(basePath, SERVICE_IMPL_PATH, EMPTY_STR).joinToString("/")
-        // dao: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】dao/
-        val mapperPath = listOf(basePath, MAPPER_PATH, EMPTY_STR).joinToString("/")
-        // mapper.xml: 【d://codeGen】/【zeta-boot】/src/main/resources/mapper_【system】
-        val xmlPath = listOf(config.outputDir, config.projectName, RESOURCES_SOURCE_PATH, MAPPER_XML_PATH, config.moduleName).joinToString("/")
-
+        val basePath = getBasePath(config)
         val pathInfos: MutableMap<OutputFile, String> = mutableMapOf(
-            OutputFile.controller to controllerPath,
-            OutputFile.entity to entityPath,
-            OutputFile.mapper to mapperPath,
-            OutputFile.mapperXml to xmlPath,
-            OutputFile.service to servicePath,
-            OutputFile.serviceImpl to serviceImplPath,
+            OutputFile.controller to getControllerPath(basePath),
+            OutputFile.entity to getEntityPath(basePath),
+            OutputFile.mapper to getMapperPath(basePath),
+            OutputFile.mapperXml to getXmlPath(config.outputDir, config.projectName, config.moduleName),
+            OutputFile.service to getServicePath(basePath),
+            OutputFile.serviceImpl to getServiceImplPath(basePath),
             OutputFile.other to basePath,
         )
 
@@ -205,64 +190,30 @@ object Generator {
      * 注入自定义配置参数  构造器
      * @return Consumer<InjectionConfig.Builder>
      */
-    private fun injectionConfigBuild(config: CodeGeneratorConfig): Consumer<InjectionConfig.Builder> = Consumer {
+    private fun injectionConfigBuild(config: CodeGeneratorConfig): Consumer<InjectionConfig.Builder> = Consumer { injectionConfigBuilder ->
         // 输出文件之前处理
-        it.beforeOutputFile { tableInfo, objectMap ->
-            log.info(JSONUtil.toJsonStr(tableInfo))
-            log.info(JSONUtil.toJsonStr(objectMap))
-
-            // 修改表名，去掉表名xxx表中的`表`字
-            val comment = StrUtil.removeSuffix(tableInfo.comment, "表")
-            tableInfo.comment = comment
-
-            // 这里也可以配置自定义模板的参数
-            val entityName = tableInfo.entityName
-            val entityDTOName = entityName + "DTO"
-            val entitySaveDTOName = entityName + "SaveDTO"
-            val entityUpdateDTOName = entityName + "UpdateDTO"
-            val entityQueryParamName = entityName + "QueryParam"
-            val lowFirstCharEntityName = entityName.lowCaseKeyFirstChar()
-            // dtoBasePackagePath：com.zeta.system.model.dto.sysUser
-            val dtoBasePackagePath = "${config.packageName}.${config.moduleName}.model.dto.${lowFirstCharEntityName}"
-            // paramBasePackagePath: com.zeta.system.model.param
-            val paramBasePackagePath = "${config.packageName}.${config.moduleName}.model.param"
-            val customTemplateParam: MutableMap<String, String> = mutableMapOf(
-                // DTO类、QueryParam类类名
-                "entityDTO" to entityDTOName,
-                "entitySaveDTO" to entitySaveDTOName,
-                "entityUpdateDTO" to entityUpdateDTOName,
-                "entityQueryParam" to entityQueryParamName,
-                // DTO类、QueryParam类包路径
-                "dtoPackagePath" to dtoBasePackagePath,
-                "paramBasePackagePath" to paramBasePackagePath,
-            )
-            objectMap["customPackage"] = customTemplateParam
+        injectionConfigBuilder.beforeOutputFile { tableInfo, objectMap ->
+            // 修改表注释，去掉表注释“xxx表”中的`表`字
+            tableInfo.comment = updateTableComment(tableInfo.comment)
 
             // 获取权限码  表名：sys_user -> sys:user
-            val authorityCode = tableInfo.name.split("_").joinToString(":")
-            objectMap["authorityCode"] = authorityCode
+            objectMap["authorityCode"] = genAuthorityCode(tableInfo.name)
 
+            // 特有的字段处理 TreeEntity、StateEntity特有字段处理
+            objectMap["uniqueColumns"] = uniqueFieldHandler(config, tableInfo.commonFields)
 
-            /**
-             * entityDTO :          【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/dto/【tableName】
-             * entitySaveDTO :      【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/dto/【tableName】
-             * entityUpdateDTO :    【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/dto/【tableName】
-             * entityQueryParam ：  【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/param
-             */
-            // entityDTOPath: model/dto/tableName
-            val entityDTOPath = "${ENTITY_DTO_PATH}/${lowFirstCharEntityName}"
-            // 在这里配置自定义模板的位置和生成出来的文件名
-            it.customFile(mutableMapOf<String, String>(
-                // ps：由于上面配置了other目录的pathInfo。所以这里的key（即文件名）要配一个路径。为什么要`../`相对路径是因为不这样会创建一个和表同名的文件夹
-                "../${entityDTOPath}/${entityDTOName}.kt" to "/templates/entityDTO.kt.btl",
-                "../${entityDTOPath}/${entitySaveDTOName}.kt" to "/templates/entitySaveDTO.kt.btl",
-                "../${entityDTOPath}/${entityUpdateDTOName}.kt" to "/templates/entityUpdateDTO.kt.btl",
-                "../${ENTITY_QUERY_PARAM_PATH}/${entityQueryParamName}.kt" to "/templates/param.kt.btl",
-            ))
+            // 自定义dto类名、包路径
+            objectMap["customPackage"] = customPackage(tableInfo.entityName, config.packageName, config.moduleName)
+
+            // 配置自定义模板的位置和生成出来的文件名
+            customFile(injectionConfigBuilder, tableInfo.entityName)
+
+            log.info(JSONUtil.toJsonStr(tableInfo))
+            log.info(JSONUtil.toJsonStr(objectMap))
         }
 
         // 在这里配置自定义的模板参数 。可以直接在模板中使用${packageName}取值
-        it.customMap(mutableMapOf<String, Any>(
+        injectionConfigBuilder.customMap(mutableMapOf<String, Any>(
             "packageName" to config.packageName,
             "timePackage" to "java.time.*",
             "repositoryAnnotation" to config.enableRepository
@@ -270,11 +221,10 @@ object Generator {
     }
 
     /**
-     * 首字母小写
+     * 【扩展函数】首字母小写
      *
      * 说明：这是一个String类的扩展函数
-     * @param key String?
-     * @return String?
+     * @return String
      */
     private fun String.lowCaseKeyFirstChar(): String {
         return if (Character.isLowerCase(this[0])) {
@@ -282,6 +232,240 @@ object Generator {
         } else {
             StringBuilder().append(Character.toLowerCase(this[0])).append(this.substring(1)).toString()
         }
+    }
+
+    /**
+     * 获取项目基础路径
+     *
+     * @param config 代码生成器配置参数
+     * @return 项目绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system
+     */
+    private fun getBasePath(config: CodeGeneratorConfig): String {
+        val packagePath = "${config.packageName.split(".").joinToString("/")}/${config.moduleName}"
+        // basePath: 【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system】
+        return listOf(config.outputDir, config.projectName, KOTLIN_SOURCE_PATH, packagePath).joinToString("/")
+    }
+
+    /**
+     * 获取Controller文件的存放路径
+     *
+     * @param basePath 项目绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system
+     * @return Controller文件的绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system/controller/
+     */
+    private fun getControllerPath(basePath: String): String {
+        return listOf(basePath, CONTROLLER_PATH, EMPTY_STR).joinToString("/")
+    }
+
+    /**
+     * 获取Entity文件的存放路径
+     *
+     * @param basePath 项目绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system
+     * @return Entity文件的绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system/model/entity/
+     */
+    private fun getEntityPath(basePath: String): String {
+        return listOf(basePath, ENTITY_PATH, EMPTY_STR).joinToString("/")
+    }
+
+    /**
+     * 获取Service文件的存放路径
+     *
+     * @param basePath 项目绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system
+     * @return Service文件的绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system/service/
+     */
+    private fun getServicePath(basePath: String): String {
+        return listOf(basePath, SERVICE_PATH, EMPTY_STR).joinToString("/")
+    }
+
+    /**
+     * 获取ServiceImpl文件的存放路径
+     *
+     * @param basePath 项目绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system
+     * @return ServiceImpl文件的绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system/service/impl/
+     */
+    private fun getServiceImplPath(basePath: String): String {
+        return listOf(basePath, SERVICE_IMPL_PATH, EMPTY_STR).joinToString("/")
+    }
+
+    /**
+     * 获取Mapper(Dao)文件的存放路径
+     *
+     * @param basePath 项目绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system
+     * @return Mapper(Dao)文件的绝对路径 eg: d://codeGen/zeta-boot/src/main/kotlin/com/zeta/system/dao/
+     */
+    private fun getMapperPath(basePath: String): String {
+        return listOf(basePath, MAPPER_PATH, EMPTY_STR).joinToString("/")
+    }
+
+    /**
+     * 获取Xml文件的存放路径
+     *
+     * @param outputDir  输出目录 不包含项目名。 eg: D://project
+     * @param projectName 项目名 eg: zeta-boot
+     * @param moduleName 模块名 eg: system
+     * @return Xml文件的绝对路径 eg: d://codeGen/zeta-boot/src/main/resources/mapper/system
+     */
+    private fun getXmlPath(outputDir: String, projectName: String, moduleName: String): String {
+        return listOf(outputDir, projectName, RESOURCES_SOURCE_PATH, MAPPER_XML_PATH, moduleName).joinToString("/")
+    }
+
+    /**
+     * 获取 EntityDTO 名称
+     *
+     * @param entityName 实体类名 eg: SysUser
+     * @return DTO名 eg:SysUserDTO
+     */
+    private fun getEntityDTOName(entityName: String): String {
+        return "${entityName}DTO"
+    }
+
+    /**
+     * 获取 SaveDTO 名称
+     *
+     * @param entityName 实体类名 eg: SysUser
+     * @return SaveDTO名 eg:SysUserSaveDTO
+     */
+    private fun getEntitySaveDTOName(entityName: String): String {
+        return "${entityName}SaveDTO"
+    }
+
+    /**
+     * 获取 UpdateDTO 名称
+     *
+     * @param entityName 实体类名 eg: SysUser
+     * @return UpdateDTO名 eg:SysUserUpdateDTO
+     */
+    private fun getEntityUpdateDTOName(entityName: String): String {
+        return "${entityName}UpdateDTO"
+    }
+
+    /**
+     * 获取 QueryParam 名称
+     *
+     * @param entityName 实体类名 eg: SysUser
+     * @return QueryParam名 eg:SysUserQueryParam
+     */
+    private fun getEntityQueryParamName(entityName: String): String {
+        return "${entityName}QueryParam"
+    }
+
+    /**
+     * 修改表注释
+     *
+     * 说明：去掉表注释“xxx表”中的`表`字
+     *
+     * @param tableComment 表注释
+     */
+    private fun updateTableComment(tableComment: String): String {
+        return if (StrUtil.isNotBlank(tableComment)) StrUtil.removeSuffix(tableComment, "表") else ""
+    }
+
+    /**
+     * 生成权限码
+     *
+     * 例如： 表名：sys_user -> sys:user
+     *
+     * @param tableName 表名
+     * @return
+     */
+    private fun genAuthorityCode(tableName: String): String {
+        return tableName.split("_").joinToString(":")
+    }
+
+    /**
+     * 特有的字段处理
+     *
+     * 说明：
+     * 像TreeEntity、StateEntity这样的类，有一些特有的字段（除了id、create_time...之外的字段）。例如：'label'、'parent_id'、'state'
+     * 在生成SaveDTO、UpdateDTO的时候不会生成进来。所以需要特别处理
+     *
+     * @param config 代码生成器配置参数
+     * @param tableInfo  表信息，关联到当前字段信息
+     */
+    private fun uniqueFieldHandler(config: CodeGeneratorConfig, commonFields: List<TableField>): List<TableField> {
+        var uniqueColumns: List<TableField> = mutableListOf()
+
+        when(config.superEntity) {
+            EntityTypeEnum.TREE_ENTITY -> {
+                uniqueColumns = commonFields.filter {
+                    // 筛选出字段名在特有字段里面的
+                    it.columnName in EntityTypeEnum.TREE_ENTITY.uniqueColumns!!.toList()
+                }
+            }
+            EntityTypeEnum.STATE_ENTITY -> {
+                uniqueColumns = commonFields.filter {
+                    // 筛选出字段名在特有字段里面的
+                    it.columnName in EntityTypeEnum.STATE_ENTITY.uniqueColumns!!.toList()
+                }
+            }
+            else -> {}
+        }
+
+        return uniqueColumns
+    }
+
+    /**
+     * 自定义dto类名、包路径
+     *
+     * 说明：
+     * 1.生成SaveDTO、UpdateDTO文件的名字
+     * 2.自定义dto、param等文件的包路径
+     *
+     * @param entityName 实体类名称 eg: SysUser
+     * @param packageName 项目包路径 eg: com.zeta
+     * @param moduleName  模块名  eg: system
+     * @return map eg: {
+     *  "entityDTO": "SysUserDTO",           // DTO文件的名字
+     *  "entitySaveDTO": "SysUserSaveDTO",   // SaveDTO文件的名字
+     *  ...
+     *  "dtoPackagePath": "com.zeta.system.model.dto.sysUser"  // DTO文件的包路径
+     *  "paramBasePackagePath": "com.zeta.system.model.param"  // QueryParam文件的包路径
+     * }
+     */
+    private fun customPackage(entityName: String, packageName: String, moduleName: String): MutableMap<String, String> {
+        // 表名首字母小写
+        val lowFirstCharEntityName = entityName.lowCaseKeyFirstChar()
+        // dtoBasePackagePath：com.zeta.system.model.dto.sysUser
+        val dtoBasePackagePath = "${packageName}.${moduleName}.model.dto.${lowFirstCharEntityName}"
+        // paramBasePackagePath: com.zeta.system.model.param
+        val paramBasePackagePath = "${packageName}.${moduleName}.model.param"
+
+        // 构造返回值map
+        return mutableMapOf(
+            // DTO类、QueryParam类类名
+            "entityDTO" to getEntityDTOName(entityName),
+            "entitySaveDTO" to getEntitySaveDTOName(entityName),
+            "entityUpdateDTO" to getEntityUpdateDTOName(entityName),
+            "entityQueryParam" to getEntityQueryParamName(entityName),
+            // DTO类、QueryParam类包路径
+            "dtoPackagePath" to dtoBasePackagePath,
+            "paramBasePackagePath" to paramBasePackagePath,
+        )
+    }
+
+    /**
+     * 配置自定义模板的位置和生成出来的文件名
+     *
+     * @param injectionConfigBuilder
+     * @param entityName 实体类名称 eg: SysUser
+     */
+    private fun customFile(injectionConfigBuilder: InjectionConfig.Builder, entityName: String) {
+        // entityDTOPath: model/dto/tableName
+        val entityDTOPath = "${ENTITY_DTO_PATH}/${entityName.lowCaseKeyFirstChar()}"
+
+        /**
+         * entityDTO :          【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/dto/【tableName】
+         * entitySaveDTO :      【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/dto/【tableName】SaveDTO
+         * entityUpdateDTO :    【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/dto/【tableName】UpdateDTO
+         * entityQueryParam ：  【d://codeGen】/【zeta-boot】/src/main/kotlin/【com/zeta/system/】/model/param
+         */
+        // 在这里配置自定义模板的位置和生成出来的文件名
+        injectionConfigBuilder.customFile(mutableMapOf<String, String>(
+            // ps：由于上面配置了other目录的pathInfo。所以这里的key（即文件名）要配一个路径。为什么要`../`相对路径是因为不这样会创建一个和表同名的文件夹
+            "../${entityDTOPath}/${getEntityDTOName(entityName)}.kt" to "/templates/entityDTO.kt.btl",
+            "../${entityDTOPath}/${getEntitySaveDTOName(entityName)}.kt" to "/templates/entitySaveDTO.kt.btl",
+            "../${entityDTOPath}/${getEntityUpdateDTOName(entityName)}.kt" to "/templates/entityUpdateDTO.kt.btl",
+            "../${ENTITY_QUERY_PARAM_PATH}/${getEntityQueryParamName(entityName)}.kt" to "/templates/param.kt.btl",
+        ))
     }
 
 }
--- src/main/kotlin/com/zeta/generator/enums/EntityTypeEnum.kt
+++ src/main/kotlin/com/zeta/generator/enums/EntityTypeEnum.kt
@@ -8,7 +8,9 @@ enum class EntityTypeEnum(
     /** 类全称，带路径 */
     val path: String,
     /** 公共字段 */
-    val columns: Array<String>
+    val columns: Array<String>,
+    /** 特有字段 */
+    val uniqueColumns: Array<String>? = null
 ) {
     /** 包括id、create_time、create_by字段的表继承的基础实体 */
     SUPER_ENTITY("org.zetaframework.base.entity.SuperEntity", arrayOf("id", "create_time", "created_by")),
@@ -17,10 +19,18 @@ enum class EntityTypeEnum(
     ENTITY("org.zetaframework.base.entity.Entity", arrayOf("id", "create_time", "created_by", "update_time", "updated_by")),
 
     /** 包括id、create_time、create_by、update_by、update_time、label、parent_id、sort_value 字段的表继承的树形实体 */
-    TREE_ENTITY("org.zetaframework.base.entity.TreeEntity", arrayOf("id", "create_time", "created_by", "update_time", "updated_by", "label", "parent_id", "sort_value")),
+    TREE_ENTITY(
+        "org.zetaframework.base.entity.TreeEntity",
+        arrayOf("id", "create_time", "created_by", "update_time", "updated_by", "label", "parent_id", "sort_value"),
+        arrayOf("label", "parent_id", "sort_value") // 特有字段，关系到SaveDTO、UpdateDTO是否生成对应的字段
+    ),
 
     /** 包括id、create_time、create_by、update_by、update_time、state字段的表继承的基础实体 */
-    STATE_ENTITY("org.zetaframework.base.entity.StateEntity", arrayOf("id", "create_time", "created_by", "update_time", "updated_by", "state")),
+    STATE_ENTITY(
+        "org.zetaframework.base.entity.StateEntity",
+        arrayOf("id", "create_time", "created_by", "update_time", "updated_by", "state"),
+        arrayOf("state") // 特有字段，关系到SaveDTO、UpdateDTO是否生成对应的字段
+    ),
 
     /** 不继承任何实体 */
     NONE("", arrayOf(""));
